82097412f0f7749f59149555962f8b0e7e85b795
Badly89 <malykhinal@mail.ru>
2021-10-15 16:00:21 +0500

    загрузка списка, функционал добавление, изменение, перешел на yarn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource, DashboardMenuItem } from "react-admin";
+import { UserList, UserEdit, UserCreate } from "./users";
+import jsonServerProvider from "ra-data-json-server";
+import authProvider from "./authProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin
+    dataProvider={dataProvider}
+    dashboard={DashboardMenuItem}
+    authProvider={authProvider}
+  >
+    <Resource
+      name="users"
+      list={UserList}
+      edit={UserEdit}
+      create={UserCreate}
+    />
+  </Admin>
+);
 
 export default App;
